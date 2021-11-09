e3f3a3061de524efcddc6d8ed49f845a72a0a994
Nael Louis <nael.louis1002@gmail.com>
2021-11-08 19:43:15 -0500

    Start load.js

---
+++ server/utils/load.js
@@ -0,0 +1,13 @@
+const csvFilePath = '../server/utils/criminalActs.csv'
+const csv = require('csvtojson')
+
+module.exports = async function load() {
+    try {
+        const jsonArray = await csv().fromFile(csvFilePath);
+        return jsonArray;
+    }
+    catch (err) {
+        console.error(err.message);
+        throw err;
+    }
+}
