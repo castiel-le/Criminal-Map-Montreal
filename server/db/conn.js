60849c4757bd80ecca6bce82c7910d68e7062210
Nael Louis <nael.louis1002@gmail.com>
2021-11-08 19:19:18 -0500

    Add conn.js and cvs dataset file

---
+++ server/db/conn.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const dbUrl = process.env.ATLAS_URI;
+const { MongoClient } = require("mongodb");
+
+//connecting to the database, creating a collection, inserting many, and disconnecting
+let instance = null;
+
+class DAO {
+    constructor() {
+        if (!instance) {
+            instance = this;
+            this.client = new MongoClient(dbUrl);
+            this.db = null;
+            this.collection = null;
+        }
+        return instance;
+    }
+
+    async connect(dbname, collName) {
+        if (this.db) {
+            return;
+        }
+        await this.client.connect();
+        this.db = await this.client.db(dbname);
+        console.log("Successfully connected to MongoDB database " + dbname);
+        this.collection = await this.db.collection(collName)
+    }
+
+    async insertMany(array) {
+        let result = await this.collection.insertMany(array);
+        return result.insertedCount;
+    }
+
+    async disconnect() {
+        this.client.close();
+    }
+}
+
+module.exports = DAO;
