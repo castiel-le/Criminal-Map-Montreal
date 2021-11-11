c88feb94ec34507f260f30e03ef8d13a0e1d9aa0
castiel <tuananhle130902@gmail.com>
2021-11-10 21:43:25 -0500

    modify load.test.js for more precise test

--- __test__/load.test.js
+++ __test__/load.test.js
@@ -3,5 +3,5 @@ const load = require("../server/utils/load")
 
 test("get json file from csv file", async () => {
     let testobj = await load()
-    expect(testobj[0]).toEqual(expect.anything())
+    expect(testobj[0]["LONGITUDE"]).toEqual("-73.62677804694519")
 })
