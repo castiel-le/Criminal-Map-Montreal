882b46b3b3ee94ad07dddf871d198afc85df2dba
Nael Louis <nael.louis1002@gmail.com>
2021-11-10 21:39:20 -0500

    Finish load function, add the filtering

--- server/utils/load.js
+++ server/utils/load.js
@@ -1,12 +1,26 @@
 const csv = require("csvtojson")
 
-module.exports = async function load() {
-  try {
-    const csvFilePath = "./server/utils/criminalActs.csv"
-    const jsonArray = await csv().fromFile(csvFilePath);
-    return jsonArray;
-  }catch (err) {
-    console.error(err.message);
-    throw err;
-  }
+module.exports = async function load(csvFilePath) {
+    try {
+        const jsonArray = await csv().fromFile(csvFilePath);
+        jsonArray.forEach((element, index, object) => {
+            let longitude = parseFloat(element["LONGITUDE"]);
+            let latitude = parseFloat(element["LATITUDE"])
+            if (longitude == 0 || latitude == 0) {
+                object.splice(index, 1);
+                return;
+            }
+
+            element.Geo = { "type": "Point", "coordinates": [longitude, latitude] };
+
+            delete element["X"];
+            delete element["Y"];
+            delete element["LATITUDE"];
+            delete element["LONGITUDE"];
+        })
+        return jsonArray;
+    } catch (err) {
+        console.error(err.message);
+        throw err;
+    }
 }
