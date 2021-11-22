44d4800182165ef7cc4245f8b9df4ceee4edf3c9
Nael Louis <nael.louis1002@gmail.com>
2021-11-22 16:07:56 -0500

    Fix findOne and add comments

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const dbUrl = process.env.ATLAS_URI;
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 //connecting to the database, creating a collection, inserting many, and disconnecting
 let instance = null;
@@ -19,12 +19,16 @@ class DAO {
 
   //connect to MongoDB
   async connect(dbname, collName) {
+    //if db exist return 
     if (this.db) {
       return;
     }
+    //Else connect to db
     await this.client.connect();
+    //Create db
     this.db = await this.client.db(dbname);
     console.log("Successfully connected to MongoDB database " + dbname);
+    //And create the collection 
     this.collection = await this.db.collection(collName)
   }
 
@@ -40,21 +44,26 @@ class DAO {
     return await this.collection.createIndex(index)
   }
 
+  //Query for one document that has the the id gotten in input
   async findOne(id) {
-    let result = await this.collection.findOne({ id: id });
+    let result = await this.collection.findOne({ "id": ObjectId(id) });
     return result;
   }
 
+  //Query for all the document in the collection
   async findAll() {
     let result = await this.collection.find();
     return result.toArray();
   }
 
-  async findPolygon(northELong, northELat, southWLong, southWLat) {
-    let point1 = [southWLong, southWLat];
-    let point2 = [southWLong, northELat];
-    let point3 = [northELong, northELat];
-    let point4 = [northELong, southWLat];
+  //Find all document in which the geo is inside the polygon 
+  async findPolygon(neLong, neLat, swLong, swLat) {
+    //Create variable as to not exceed the number of character possible in a line
+    let point1 = [swLong, swLat];
+    let point2 = [swLong, neLat];
+    let point3 = [neLong, neLat];
+    let point4 = [neLong, swLat];
+    //Query to find all document in which the geo respect the query
     let result = await this.collection.find({
       geo: {
         $geoWithin:
@@ -66,6 +75,7 @@ class DAO {
         }
       }
     });
+    //return the array
     return result.toArray();
   }
   //disconnect from MongoDB
