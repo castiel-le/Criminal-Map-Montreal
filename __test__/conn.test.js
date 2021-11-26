956b6089d8c951997a6daa2ff95b9eab60d4f8d5
Nael Louis <nael.louis1002@gmail.com>
2021-11-25 21:46:30 -0500

    Add attempt at mocking conn

---
+++ __test__/conn.test.js
@@ -0,0 +1,16 @@
+const mock = require("jest");
+const request = require("supertest")
+const route = require("../server/routes/root");
+const DAO = require("../server/db/conn");
+
+jest.mock("../server/db/conn");
+
+describe("Get a document from the db", () => {
+    test("Should return only one document", async () => {
+        let resultDoc = { "_id": "619bd1864238d0d7f75994e9", "CATEGORIE": "Vol de véhicule à moteur", "DATE": "2020-10-12", "QUART": "nuit", "PDQ": "49", "Geo": { "type": "Point", "coordinates": [-73.49771442385476, 45.67852431721866] } };
+        jest.spyOn(DAO.prototype, "findSingleCase").mockResolvedValue(resultDoc);
+        let response = await request(route).get("/:id").query("619bd1864238d0d7f75994e9");
+        expected(response.json).toMatch(resultDoc);
+        expected(response.statusCode).toEqual(200);
+    });
+});
