b07cc20a2d8678bd6c07fbb4a9503df5753ccd37
Nael Louis <nael.louis1002@gmail.com>
2021-11-25 21:19:21 -0500

    eslint fix

--- __test__/load.test.js
+++ __test__/load.test.js
@@ -2,28 +2,28 @@ const { test, expect } = require("@jest/globals")
 const load = require("../server/utils/load.js")
 const path = "./__test__/test.csv"
 test("get json file from csv file", async () => {
-    let testobj = await load(path)
-    console.log(testobj[0])
+    let testObj = await load(path)
+    console.log(testObj[0])
     let expected = { "CATEGORIE": "Méfait", "DATE": "2021-04-12", "QUART": "soir", "PDQ": "50", "Geo": { "type": "Point", "coordinates": [-73.62308621281647, 45.49686399748815] } }
-    expect(testobj[0]).toEqual(expected)
+    expect(testObj[0]).toEqual(expected)
 })
 
 test("validate the length of the array", async () => {
-    let testobj = await load(path)
-    console.log(testobj)
-    expect(testobj.length).toEqual(2)
+    let testObj = await load(path)
+    console.log(testObj)
+    expect(testObj.length).toEqual(2)
 })
 
-test("file does not exist", async () =>{
+test("file does not exist", async () => {
     expect.assertions(1)
-    try{
-        let testobj = await load("randompath")
-    }catch(e){
+    try {
+        let testObj = await load("randompath")
+    } catch (e) {
         expect(e.message).toMatch("File does not exist")
     }
 })
 
 test("wrong format file", async () => {
-        let testobj = await load("./__test__/random.txt")
-        expect(testobj).toEqual([])
+    let testObj = await load("./__test__/random.txt")
+    expect(testObj).toEqual([])
 })
