7ff7a4817d4b866fd56db0a00aeb0756468be806
Nael Louis <nael.louis1002@gmail.com>
2021-12-06 20:59:19 -0500

    Start config parameters

---
+++ client/src/utils/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    attribution: "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors",
+    tileUrl: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    startCenter: [45.558294879426235, -73.69354248046875],
+    startBounds: [[45.413394446947535, -73.96751403808592], [45.71672752568247, -73.42369079589844]],
+    initialZoom: ,
+    minZoom: ,
+    maxZoom: ,
+};
