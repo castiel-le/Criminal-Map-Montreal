b0d20e21368bc22f7495487ecbb326bfada4967e
Nael Louis <nael.louis1002@gmail.com>
2021-12-10 19:08:48 +0000

    Revert "Revert "Merge branch 'mainmap' into 'Project'""
    This reverts commit dcf636dcaa4c78c6eaa80bd707e499ddc48e0b54

--- client/src/components/CrimeTooltip.js
+++ client/src/components/CrimeTooltip.js
@@ -4,14 +4,33 @@ import {Component} from "react";
 export default class CrimeTooltip extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      crime: "",
+    }
+    this.fetchDocument = this.fetchDocument.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.fetchDocument();
+  }
+
+  async fetchDocument() {
+    let response = await fetch(`/case/${this.props.selected._id}`);
+    let data = await response.json();
+    if (data.statusCode === 404) {
+      console.log(data.statusCode);
+      return;
+    }
+    this.setState({ crime: data });
+    console.log(this.state.crime);
   }
 
   render() {
     return (
       <div>
-        <p><b>{this.props.selected.CATEGORIE}</b></p>
-        <p>{this.props.selected.DATE}, {this.props.selected.QUART}</p>
-        <p>PDQ: {this.props.selected.PDQ}</p>
+        <p><b>{this.state.crime.CATEGORIE}</b></p>
+        <p>{this.state.crime.DATE}, {this.state.crime.QUART}</p>
+        <p>PDQ: {this.state.crime.PDQ}</p>
       </div>
     );
   }
